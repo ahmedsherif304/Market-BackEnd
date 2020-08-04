13d0f7f3412bb313405092ab0ce27d557a21d4f5
ahmedsherif304 <ahmedsamora24@yahoo.com>
2020-08-04 22:59:30 +0200

    product buyer middleware for rating products

--- middleware/productBuyer.js
+++ middleware/productBuyer.js
@@ -1,9 +1,11 @@
-
+const {Order} = require('../models/order');
+const {validate} = require('../models/rate');
 
 module.exports = async function(req,res,next)
 {
     try{
-        
+        const order = await Order.find({userId:req.body.userToken._id,products:{id:req.params.productId}});
+        if (!order)   return res.status(400).send('you cannot rate this product you have to buy it first');
         next();
     }
     catch{ex}
