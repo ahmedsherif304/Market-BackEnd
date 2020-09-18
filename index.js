d6c7320913c5e461404f80f0deace8d710611a9a
ahmedsherif304 <ahmedsamora24@yahoo.com>
2020-09-18 17:59:27 +0200

    setting envirnoment variable

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const morgan = require("morgan");
 
 require("./startup/logging")();
-require("./startup/config")();
 require("./startup/db")();
+require("./startup/config")();
 require("./startup/routes")(app);
 
 if (app.get("env") === "development") app.use(morgan("tiny"));
 else require("./startup/prod")(app);
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => winston.info(`listening on port ${port}...`));
