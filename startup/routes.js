34d88d94430c405ea232c95508277315aefe5cf6
ahmedsherif304 <ahmedsamora24@yahoo.com>
2020-09-21 05:07:38 +0200

    allowing the access to the backend from any source

--- startup/routes.js
+++ startup/routes.js
@@ -13,6 +13,14 @@ const orders = require("../routes/orders");
 
 module.exports = (app) => {
   app.use(express.json());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use("/users", users);
   app.use("/", home);
   app.use("/login", login);
